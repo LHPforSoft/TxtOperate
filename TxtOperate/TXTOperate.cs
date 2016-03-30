30d094c360e182c2c7d460989bba3456adb4d2c4
LHPForSoft <mail_mrliu@163.com>
2016-03-30 15:34:04 +0800

    aaa

---
+++ TxtOperate/TXTOperate.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TxtOperate
+{
+    class TXTOperate
+    {
+        public void OpenFile()
+        {
+            //
+        }
+        public void SaveFile()
+        {
+            //
+        }
+
+        public void ReadText()
+        {
+            //
+        }
+        public void AddText()
+        {
+            //
+        }
+
+
+
+    }
+}
